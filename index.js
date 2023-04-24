d9bf7e95aa67a3ff067279742b95a7f30481daf5
Sayem92 <sayemmiha123@gmail.com>
2023-04-24 22:01:27 +0600

    delete api

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ async function run() {
       );
       res.send(result);
     });
+    // delete reminder
+    app.delete("/reminder/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reminderCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 }
